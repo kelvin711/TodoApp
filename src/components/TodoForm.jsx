2e2e14c2f092a052540740cc7f1b11543f6eba54
kelvin711 <60142095+kelvin711@users.noreply.github.com>
2021-02-03 18:42:03 -0600

    added state and form input on todoform

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,28 @@
 import React, { useState }  from 'react';
 
 const TodoForm = () => {
+//State
+    const [formInput, setFormInput] = useState("");
+
+
     return (
-        <div>
-            
-        </div>
+        <form className="todo-form">
+
+            <input 
+            className="todo-input"
+            type="text" 
+            name="text" 
+            placeholder="Add a todo"
+            value={formInput}
+            />
+
+            <button 
+            className="todo-button"
+            >
+                Add todo
+            </button>
+
+        </form>
     );
 };
 
